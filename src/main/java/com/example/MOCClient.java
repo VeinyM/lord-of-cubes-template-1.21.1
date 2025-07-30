3f3a710fd367b7d2e8ebabe655d038538c05ef95
Zhaiyk <Zhaiykelubay@gmail.com>
2025-07-30 12:40:00 +0500

    0.1 version. only getting started!

---
+++ src/main/java/com/example/MOCClient.java
@@ -0,0 +1,11 @@
+package com.example;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class MOCClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
